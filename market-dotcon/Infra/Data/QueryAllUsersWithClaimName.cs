d995fdccfdace3b07a998dc2dbb0ae382d4c99c4
Babisque <rodrigobabisque1@gmail.com>
2023-04-11 19:18:32 -0300

    dapper added to becoming better the performance

---
+++ market-dotcon/Infra/Data/QueryAllUsersWithClaimName.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using market_dotcon.Endpoints.Employees;
+using Microsoft.Data.SqlClient;
+
+namespace market_dotcon.Infra.Data;
+
+public class QueryAllUsersWithClaimName
+{
+    private readonly IConfiguration cfg;
+
+    public QueryAllUsersWithClaimName(IConfiguration cfg)
+    {
+        this.cfg = cfg;
+    }
+
+    public IEnumerable<EmployeeResponse> Execute(int page, int rows)
+    {
+        var db = new SqlConnection(cfg["ConnectionString:DotConDb"]);
+        return db.Query<EmployeeResponse>(
+            @"SELECT Email, ClaimValue AS Name
+              FROM AspNetUsers u INNER JOIN AspNetUserClaims c
+              ON u.Id = c.UserId AND c.ClaimType = 'Name'
+              ORDER BY name
+              OFFSET (@page - 1) * @rows ROWS FETCH NEXT @rows ROWS ONLY;",
+            new { page, rows }
+            );
+    }
+}
--- market-dotcon/Program.cs
+++ market-dotcon/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
 {
     options.Password.RequiredLength = 8;
 }).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddScoped<QueryAllUsersWithClaimName>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
